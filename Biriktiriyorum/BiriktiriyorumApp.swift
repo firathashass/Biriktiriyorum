41cd3bb90b2dfb8d058a0a873c7b7b9ba5d8c56b
firathashass <firathashass@gmail.com>
2025-07-12 21:37:13 +0300

    Initial Commit

---
+++ Biriktiriyorum/BiriktiriyorumApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiriktiriyorumApp.swift
+//  Biriktiriyorum
+//
+//  Created by Fırat Haşhaş on 12.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct BiriktiriyorumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiriktiriyorumTests/BiriktiriyorumTests.swift
@@ -0,0 +1,17 @@
+//
+//  BiriktiriyorumTests.swift
+//  BiriktiriyorumTests
+//
+//  Created by Fırat Haşhaş on 12.07.2025.
+//
+
+import Testing
+@testable import Biriktiriyorum
+
+struct BiriktiriyorumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
